5b00a8ba428c86d1d5b70bd83bba272c588c3c3b
John <melon42@gmail.com>
2014-09-29 10:27:31 -0400

    reward change

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,25 +13,25 @@ class User < ActiveRecord::Base
     REFERRAL_STEPS = [
         {
             'count' => 5,
-            "html" => "50% OFF <br> 1st Month Subscription on Signup",
+            "html" => "10% OFF <br> 1st Meal on Signup",
             "class" => "two",
             "image" =>  "http://thesavorychronicles.files.wordpress.com/2013/05/vertical.jpg"
         },
         {
             'count' => 10,
-            "html" => "1 Free Meal <br> on Signup",
+            "html" => "30% OFF <br> 1st Meal on Signup",
             "class" => "three",
             "image" => "http://24.media.tumblr.com/tumblr_mck8eqJ2101rcjq6vo1_500.png"
         },
         {
             'count' => 25,
-            "html" => "2 Free Meals <br> on Signup",
+            "html" => "1 Free Meals <br> on Signup",
             "class" => "four",
             "image" => "http://24.media.tumblr.com/tumblr_m98h8bHn751rcjq6vo1_250.png"
         },
         {
             'count' => 50,
-            "html" => "1 Month of Free Meals <br> on Signup",
+            "html" => "2 Free Meals <br> on Signup",
             "class" => "five",
             "image" => "http://thumbs.dreamstime.com/z/cooked-crabs-food-8471152.jpg"
         }
